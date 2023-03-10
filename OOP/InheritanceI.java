05d0e48b79bae168c4fecec42b3b157e0c9ff604
AhmedBoshra <ahmedboshra96@gmail,com>
2023-03-10 21:22:56 +0200

    uploading problems

--- OOP/InheritanceI.java
+++ OOP/InheritanceI.java
@@ -1,4 +1,15 @@
 package OOP;
 
 public class InheritanceI {
+    class Arithmetic {
+        int x,y;
+        public static int add(int x, int y) {
+            return x + y;
+        }
+    }
+    class Adder extends Arithmetic {
+
+    }
 }
+
+// new version Hackerrank
